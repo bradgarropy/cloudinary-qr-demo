42b3002e30679ae6f2e55b4355ebc48a87fb2e78
Brad Garropy <bgarropy@atlassian.com>
2024-04-04 15:33:19 -0500

    first pass.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import {sayHello} from "./hello"
+import "dotenv/config"
 
-sayHello()
+import {v2 as cloudinary} from "cloudinary"
+
+const input = process.argv.at(-1) ?? ""
+
+cloudinary.config({
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: "bradgarropy",
+    secure: true,
+    force_version: false,
+})
+
+const url = cloudinary.url(`cloudinary-qr-demo/${input}`, {
+    effect: "vectorize",
+    format: "svg",
+})
+
+console.log(url)
