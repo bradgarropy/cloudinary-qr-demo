dadcb09ed9b8ec38eba9aff4cc25fba8e1c9f1b7
Brad Garropy <bgarropy@atlassian.com>
2024-05-03 11:49:58 -0500

    remove cli arguments.

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,6 @@ import "dotenv/config"
 
 import {v2 as cloudinary} from "cloudinary"
 
-if (process.argv.length < 3) {
-    console.log("Usage: npm start <url> [color]")
-    process.exit(1)
-}
-
-let url
-let color
-
-if (process.argv.length === 3) {
-    url = process.argv.at(-1)
-}
-
-if (process.argv.length === 4) {
-    url = process.argv.at(-2)
-    color = process.argv.at(-1)
-}
-
 cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
@@ -27,13 +10,12 @@ cloudinary.config({
     force_version: false,
 })
 
-const cloudinaryUrl = cloudinary.url(`cloudinary-qr-demo/${url}`, {
-    fetch_format: "svg",
-    background: color ? color : "white",
+const cloudinaryUrl = cloudinary.url("cloudinary-qr-demo/bradgarropy.com", {
     transformation: [
-        color ? {effect: `replace_color:${color}`} : undefined,
         {effect: "make_transparent"},
-        {effect: "vectorize"},
+        // {effect: "replace_color:red"},
+        // {background: "blue"},
+        {effect: "vectorize", fetch_format: "svg"},
     ],
 })
 
